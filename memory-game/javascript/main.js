6b4509f792be3a79589589506fe72ce8cacc0d65
Noel Abellera <noel@noels-mbp.lan>
2017-10-26 02:19:32 -0700

    created game logic

--- memory-game/javascript/main.js
+++ memory-game/javascript/main.js
@@ -1,20 +1,26 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2){
+var checkForMatch = function (){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
-	}else {
+		}else {
 		alert("Sorry, Try Again!");
-	}
+		}
 }
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function (cardId){
+	console.log("User flipped " +  cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
+	
+}
+
+flipCard(0);
+flipCard(2);
